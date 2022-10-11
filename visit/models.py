8ae2187aded0767417f574b6268e2a733915780f
Miko55562 <42392940+Miko55562@users.noreply.github.com>
2022-10-12 02:19:19 +0300

    db

--- visit/models.py
+++ visit/models.py
@@ -17,7 +17,7 @@ class Category(models.Model):
 
 class Subcategory(models.Model):
     title = models.CharField(max_length=150, db_index=True, verbose_name='Подкатегория')
-    parent_category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.title
@@ -32,45 +32,45 @@ class Subcategory(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=150, verbose_name='Название')
 
-    product_code = models.CharField(max_length=150, verbose_name='Код Товара 1', null=True)
-    weight = models.CharField(max_length=150, verbose_name='Вес продукции 1', null=True)
+    product_code = models.CharField(max_length=150, verbose_name='Код Товара 1', null=True, blank=True)
+    weight = models.CharField(max_length=150, verbose_name='Вес продукции 1', null=True, blank=True)
 
-    product_code_2 = models.CharField(max_length=150, verbose_name='Код Товара 2', null=True)
-    weight_2 = models.CharField(max_length=150, verbose_name='Вес продукции 2', null=True)
+    product_code_2 = models.CharField(max_length=150, verbose_name='Код Товара 2', null=True, blank=True)
+    weight_2 = models.CharField(max_length=150, verbose_name='Вес продукции 2', null=True, blank=True)
 
-    product_code_3 = models.CharField(max_length=150, verbose_name='Код Товара 3', null=True)
-    weight_3 = models.CharField(max_length=150, verbose_name='Вес продукции 3', null=True)
+    product_code_3 = models.CharField(max_length=150, verbose_name='Код Товара 3', null=True, blank=True)
+    weight_3 = models.CharField(max_length=150, verbose_name='Вес продукции 3', null=True, blank=True)
 
-    product_code_4 = models.CharField(max_length=150, verbose_name='Код Товара 4', null=True)
-    weight_4 = models.CharField(max_length=150, verbose_name='Вес продукции 4', null=True)
+    product_code_4 = models.CharField(max_length=150, verbose_name='Код Товара 4', null=True, blank=True)
+    weight_4 = models.CharField(max_length=150, verbose_name='Вес продукции 4', null=True, blank=True)
 
-    product_code_5 = models.CharField(max_length=150, verbose_name='Код Товара 5', null=True)
-    weight_5 = models.CharField(max_length=150, verbose_name='Вес продукции 5', null=True)
+    product_code_5 = models.CharField(max_length=150, verbose_name='Код Товара 5', null=True, blank=True)
+    weight_5 = models.CharField(max_length=150, verbose_name='Вес продукции 5', null=True, blank=True)
 
-    product_code_6 = models.CharField(max_length=150, verbose_name='Код Товара 6', null=True)
-    weight_6 = models.CharField(max_length=150, verbose_name='Вес продукции 6', null=True)
+    product_code_6 = models.CharField(max_length=150, verbose_name='Код Товара 6', null=True, blank=True)
+    weight_6 = models.CharField(max_length=150, verbose_name='Вес продукции 6', null=True, blank=True)
 
-    product_sku = models.CharField(max_length=150, verbose_name='Артикул товара', null=True)
+    product_sku = models.CharField(max_length=150, verbose_name='Артикул товара', null=True, blank=True)
 
-    manufacturer = models.CharField(max_length=150, verbose_name='Производитель', null=True)
+    manufacturer = models.CharField(max_length=150, verbose_name='Производитель', null=True, blank=True)
 
     description = models.TextField(blank=True, verbose_name='Описание', null=True)
 
     # Характеристики
-    country_of_origin = models.CharField(max_length=150, verbose_name='Страна производства', null=True)
-    composition = models.CharField(max_length=150, verbose_name='Состав', null=True)
-    nutritional_value_of_100_grams = models.CharField(max_length=150, verbose_name='Пищевая ценность 100 грамм', null=True)
-    energy_value = models.CharField(max_length=150, verbose_name='Энергетическая ценность', null=True)
-    best_before_date = models.CharField(max_length=150, verbose_name='Срок годности', null=True)
-    type_of_packaging = models.CharField(max_length=150, verbose_name='Тип упаковки', null=True)
-    price_is_for = models.CharField(max_length=150, verbose_name='Цена указана за', null=True)
+    country_of_origin = models.CharField(max_length=150, verbose_name='Страна производства', null=True, blank=True)
+    composition = models.TextField(max_length=355, verbose_name='Состав', null=True, blank=True)
+    nutritional_value_of_100_grams = models.CharField(max_length=150, verbose_name='Пищевая ценность 100 грамм', null=True, blank=True)
+    energy_value = models.CharField(max_length=150, verbose_name='Энергетическая ценность', null=True, blank=True)
+    best_before_date = models.CharField(max_length=150, verbose_name='Срок годности', null=True, blank=True)
+    type_of_packaging = models.CharField(max_length=150, verbose_name='Тип упаковки', null=True, blank=True)
+    price_is_for = models.CharField(max_length=150, verbose_name='Цена указана за', null=True, blank=True)
 
     availability = models.BooleanField(default=True, verbose_name='Наличие товара')
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
 
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
     subcategory =  models.ForeignKey(Subcategory, on_delete=models.CASCADE, verbose_name='Подкатегория')
-    
+
 
     def __str__(self):
         return self.name
