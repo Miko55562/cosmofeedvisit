09e56bdb23cc253f92555ec67af887ba26201236
Miko55562 <42392940+Miko55562@users.noreply.github.com>
2022-10-14 00:13:22 +0300

    download

--- visit/urls.py
+++ visit/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('category/<slug:category_slug>', catalog_category, name='catalog_category'),
     path('product/<int:product_pk>', product_big, name='product_big'),
     path('category/<slug:category_slug>/subcategory/<slug:subcategory_slug>', catalog_subcategory, name='catalog_subcategory'),
-    path("robots.txt", robots),
+    path('robots.txt', robots),
 ]
--- visit/views.py
+++ visit/views.py
@@ -5,13 +5,31 @@ from .models import Subcategory
 from .models import ContactForm
 from .models import Malling
 from .models import CaruselProduct
-from django.core.mail import send_mail
+from .models import CatalogFile
 
+from django.core.mail import send_mail
+import os
+from django.conf import settings
+from django.http import HttpResponse, Http404
 
 def robots(request):
     return render(request, template_name='visit/robots.txt', content_type="text/plain")
 
+def download(request):
+    print(request)
+    path = CatalogFile.objects.get(id=1)
+    file_path = os.path.join(settings.MEDIA_ROOT, str(path.file))
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as fh:
+            response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")
+            response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+            return response
+    raise Http404
+
 def index(request):
+    if request.method == 'GET' and request.GET.get('download'):
+        return download(request)
+
     if request.method == 'POST' and request.POST.get('ContactForm'):
         request_name = request.POST.get("name")
         request_mail = request.POST.get("mail")
@@ -28,7 +46,6 @@ def index(request):
     categories = Category.objects.all()
     subcategories = Subcategory.objects.all()
     carusel_product = CaruselProduct.objects.order_by('product')
-    print(carusel_product)
     context = {
     'categories': categories,
     'carusel_product': carusel_product,
@@ -36,6 +53,9 @@ def index(request):
     return render(request, template_name='visit/index.html', context=context)
 
 def category(request, category_id):
+    if request.method == 'GET' and request.GET.get('download'):
+        return download(request)
+
     subcategory = Subcategory.objects.filter(category_id = category_id)
     context = {
     'subcategory': subcategory,
@@ -46,6 +66,9 @@ def category(request, category_id):
     return render(request, template_name='visit/catalog.html', context=context)
 
 def catalog_category(request, category_slug):
+    if request.method == 'GET' and request.GET.get('download'):
+        return download(request)
+
     categories = Category.objects.all()
     category = Category.objects.get(slug=category_slug)
     products = Product.objects.filter(category_id = category.id)
@@ -59,6 +82,9 @@ def catalog_category(request, category_slug):
     return render(request, template_name='visit/catalog_subcat.html', context=context)
 
 def catalog_subcategory(request, category_slug, subcategory_slug):
+    if request.method == 'GET' and request.GET.get('download'):
+        return download(request)
+
     categories = Category.objects.all()
     category = Category.objects.get(slug=category_slug)
     subcategory = Subcategory.objects.get(slug=subcategory_slug)
@@ -75,6 +101,9 @@ def catalog_subcategory(request, category_slug, subcategory_slug):
     return render(request, template_name='visit/catalog_subcat.html', context=context)
 
 def catalog(request):
+    if request.method == 'GET' and request.GET.get('download'):
+        return download(request)
+
     categories = Category.objects.all()
     context = {
     'categories': categories,
@@ -82,6 +111,9 @@ def catalog(request):
     return render(request, template_name='visit/catalog.html', context=context)
 
 def delivery(request):
+    if request.method == 'GET' and request.GET.get('download'):
+        return download(request)
+
     categories = Category.objects.all()
     subcategories = Subcategory.objects.all()
     context = {
@@ -90,6 +122,9 @@ def delivery(request):
     return render(request, template_name='visit/delivery.html', context=context)
 
 def product_big(request, product_pk):
+    if request.method == 'GET' and request.GET.get('download'):
+        return download(request)
+
     product = Product.objects.get(pk=product_pk)
     context = {
     'product':product
